4d457ef4a117e26ffa03c79856d6e578800810db
laureanomarenco <laureanomarenco@gmail.com>
2022-09-10 16:33:48 -0300

    first commit tsc

--- build/src/app.js
+++ build/src/app.js
@@ -1,5 +1,6 @@
 "use strict";
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
