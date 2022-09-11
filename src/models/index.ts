00579656436da9a91c916a526599416e65a9d842
laureanomarenco <laureanomarenco@gmail.com>
2022-09-11 19:29:09 -0300

    first commit

--- src/models/index.ts
+++ src/models/index.ts
@@ -12,12 +12,7 @@ let sequelize: any;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+  sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}/masco`);
 }
 
 fs.readdirSync(__dirname)
