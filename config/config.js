6c657df947668e755fd84a158a79dd434d13294e
laureanomarenco <laureanomarenco@gmail.com>
2022-09-11 20:23:32 -0300

    first-commit

--- config/config.js
+++ config/config.js
@@ -22,6 +22,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    use_env_variable: "DATABASE_URL"
+    use_env_variable: "DATABASE_URL",
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
