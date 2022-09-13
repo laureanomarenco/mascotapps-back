be669f6fd25aa3af4d633bd539c6ca00734831fc
Robert0101 <rob.tangled@gmail.com>
2022-09-13 01:12:23 -0300

    refactor/Cambiar nombre del archivo. Actualizar nombre del Model, de PetTS a Animal

---
+++ src/routes/pets.ts
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import db from "../../models/index";
+import { validateNewPet } from "../auxiliary/AnimalValidators";
+import { Pet } from "../types/petTypes";
+// import { Ages, Genders, Pet, Species, Status } from "../types/petTypes";
+
+const router = Router();
+
+// ----- ------ ------ FUNCIONES AUXILIARES PARA LAS RUTAS: ------- -------- --------
+
+const getAllPets = async () => {
+  try {
+    const allPets = await db.Animal.findAll();
+    // console.log(allPets);
+    return allPets;
+  } catch (error: any) {
+    console.log(error.message);
+    return error;
+  }
+};
+
+// ----- ------ ------- RUTAS :  ------ ------- -------
+
+router.get("/", async (_req, res) => {
+  console.log("entré al get de pets!");
+
+  try {
+    let allThePets = await getAllPets();
+    // console.log(allThePets);
+
+    return res.status(200).send(allThePets);
+  } catch (error: any) {
+    return res.status(404).send(error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  console.log("entré al POST de Animal!");
+  try {
+    let validatedPet: Pet = validateNewPet(req.body);
+    console.log("SOY VALIDATED PET: ");
+    console.log(validatedPet);
+
+    let createdPet = await db.Animal.create(validatedPet);
+    return res.status(200).send(createdPet);
+  } catch (error: any) {
+    return res.status(404).send(error.message);
+  }
+});
+
+export default router;
