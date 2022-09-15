dca3e48a1fda7fa027a901bd959ad94576bca749
Robert0101 <rob.tangled@gmail.com>
2022-09-15 13:29:55 -0300

    build/Integrar PassportPJ al código

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import usersRouter from "./routes/users";
 import animalRouter from "./routes/pets";
+//! ---- nuevo para passport:
+const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
+const passportSetup = require("../config/passport-setup");
+const keys = require("../config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+//!---fin nuevo para passport ----
 // import db from "./src/models";
 const app = express();
 
 app.use(express.json()); // middleware que transforma la req.body a un json
-
 app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
@@ -17,14 +24,39 @@ app.use((_req, res, next) => {
   next();
 });
 
+//ruta para testear que responde la api:
 app.get("/ping", (_req, res) => {
   // le puse el guión bajo al req para decirle a typescript que ignore el hecho de que no uso esa variable req.
   console.log("Someone pinged here!!!");
   res.send("pong");
 });
 
+//! set up view engine. No debería estar, pero lo pongo para testeos provisorios:
+app.set("view engine", "ejs");
+
+// middlewares para encriptar la cookie que voy a enviar al browser:
+app.use(
+  cookieSession({
+    maxAge: 1000 * 60 * 2, // === dos minutos
+    keys: [keys.session.cookieKey],
+  })
+);
+
+//Inicializar passport:
+app.use(passport.initialize());
+app.use(passport.session());
+
+// RUTAS:
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 app.use("/users", usersRouter);
 app.use("/pets", animalRouter);
+
+app.get("/", (req: any, res) => {
+  console.log("ENTRÉ AL GET DE '/' y el req.user es " + req.user);
+
+  res.render("home", { usuario: req.user });
+});
 
 module.exports = app;
 
---
+++ src/routes/auth-routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+//auth login:
+router.get("/login", (req, res) => {
+  res.render("login", { usuario: req.user });
+});
+
+//auth logout
+router.get("/logout", (req, res) => {
+  //#20: simplemente un req.logout(), lo cual destruye la parte del id encriptado de la cookie, y después redirijo
+  req.logout();
+  res.redirect("/");
+});
+
+//auth with google:
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    //el scope le dice a passport qué datos del perfil del usuario queremos obtener
+    scope: ["profile"], //acá uno dice que no tiene que estar adentro de un array si quiero poner más de dos en scope, si no que tiene que ser scope: "profile email", successRedirect: "/", failureRedirect: "/login"
+  })
+);
+
+// callback route for google to redirect to
+// entra a esta ruta cuando google me redirecciona, completando la URL/URI con el code que completan en la query de la URL
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  console.log("ESTOY DESPUÉS DEL MIDDLEWARE DE AUTHTENTICATE");
+  //#17 Ahora en este req me va a llegar el user en req.user:
+  // res.send(req.user);
+  // res.send("you reached the callback URI");
+  //#18 Voy a redirigir y enviar al cliente a una URL particular. Por ejemplo, a /profile. Hago un archivo con las rutas para el perfil.
+  res.redirect("/profile/");
+});
+
+module.exports = router;
