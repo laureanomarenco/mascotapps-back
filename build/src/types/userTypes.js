78b3d3755ce1f923962807b80030bf126e238bc8
Robert0101 <rob.tangled@gmail.com>
2022-09-17 10:49:06 -0300

    nuevo tsc

---
+++ build/src/types/userTypes.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ESTA INTERFACE ESTÁ EN DESUSO YA QUE SE USA DIRECTAMENTE EN EL ../models/user.ts, pero hice una copia de la que está en models y la traje para acá así la importo a donde quiera y la puedo usar de comparación de types.
