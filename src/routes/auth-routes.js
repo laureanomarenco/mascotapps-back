68d2e099037f8972559744891f996e697b75cc12
Robert0101 <rob.tangled@gmail.com>
2022-09-17 15:01:53 -0300

    Agregar clogs

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -29,7 +29,7 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   console.log("ESTOY DESPUÉS DEL MIDDLEWARE DE AUTHTENTICATE");
   //#17 Ahora en este req me va a llegar el user en req.user:
   // res.send(req.user);
-  res.send("you reached the callback URI");
+  res.send(`you reached the callback URI, ${req.user.displayName} `);
   //#18 Voy a redirigir y enviar al cliente a una URL particular. Por ejemplo, a /profile. Hago un archivo con las rutas para el perfil.
   // res.redirect("/profile/");
 });
