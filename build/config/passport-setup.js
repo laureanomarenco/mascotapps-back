b3126723c77e374de054932d7f9aec34941f6f70
Robert0101 <rob.tangled@gmail.com>
2022-09-17 11:24:31 -0300

    tsc

--- build/config/passport-setup.js
+++ build/config/passport-setup.js
@@ -42,8 +42,8 @@ passport.use(new GoogleStrategy({
     //options for the strategy
     callbackURL: "/auth/google/redirect",
     // Yo voy a agarrar ese código y se lo voy a intercambiar a google por datos del user profile. Y una vez que me trae esos datos, se ejecuta el passport callback function de esta función (segundo argumento)
-    clientID: config.production.clientID,
-    clientSecret: config.production.clientSecret,
+    clientID: config.clientID,
+    clientSecret: config.clientSecret,
     // clientID: keys.google.clientID,
     // clientSecret: keys.google.clientSecret,
 }, (accessToken, refreshToken, profile, done) => __awaiter(void 0, void 0, void 0, function* () {
