738d2a05f1c83cc4b29c266c807ec7f5bbfd58c7
Robert0101 <rob.tangled@gmail.com>
2022-09-17 15:18:29 -0300

    hacer redirect a profile

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -29,7 +29,8 @@ router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   console.log("ESTOY DESPUÉS DEL MIDDLEWARE DE AUTHTENTICATE");
   //#17 Ahora en este req me va a llegar el user en req.user:
   // res.send(req.user);
-  res.send(`you reached the callback URI, ${req.user.displayName} `);
+  res.redirect("/profile/");
+  // res.send(`you reached the callback URI, ${req.user.displayName} `);
   //#18 Voy a redirigir y enviar al cliente a una URL particular. Por ejemplo, a /profile. Hago un archivo con las rutas para el perfil.
   // res.redirect("/profile/");
 });
