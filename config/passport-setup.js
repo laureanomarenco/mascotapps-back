8b5b191119d5d028918bcf976dc1e5d9a38f0eed
Robert0101 <rob.tangled@gmail.com>
2022-09-17 11:18:45 -0300

    comentar dotenv

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/config.js")[env];
-require("dotenv").config();
+// require("dotenv").config();
 import db from "../models/index";
 // import {UserAttributes} from ("../src/types/userTypes")
 
