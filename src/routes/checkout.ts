8db46e05ef5e056301cde38fdac77ec6209d5816
laureanomarenco <laureanomarenco@gmail.com>
2022-09-18 18:34:06 -0300

    refact

--- src/routes/checkout.ts
+++ src/routes/checkout.ts
@@ -30,36 +30,4 @@ router.post('/', async (req, res) => {
         res.json({msg: err.raw.message})
     }
 })
-
-
-  const endpointSecret = "whsec_da7m5mm9xQTKTJOOAJ8sqa0ry0pZoriH";
-
-  router.post('/balance', (request, response) => {
-    const sig = request.headers['stripe-signature'];
-  
-    let event;
-  
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err: any) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-  
-    // Handle the event
-    switch (event.type) {
-      case 'balance.available':
-        const balance = event.data.object;
-        // Then define and call a function to handle the event balance.available
-        response.send(balance)
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-  
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
-  })
-
 export default router;
