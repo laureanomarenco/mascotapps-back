739c43aa3ead03d42976780104bd26c3bcd63eba
laureanomarenco <laureanomarenco@gmail.com>
2022-09-19 14:56:27 -0300

    nodemailer

--- src/routes/checkout.ts
+++ src/routes/checkout.ts
@@ -5,12 +5,13 @@ const Stripe = require('stripe')
 const router = Router();
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "../../../config/config.js")[env];
+const { GMAIL_PASS, GMAIL_USER } = process.env
 
 let stripe: any;
 if(config.stripeKeyProd){
-    stripe = new Stripe(process.env[config.stripeKeyProd])
+  stripe = new Stripe(process.env[config.stripeKeyProd])
 } else {
-    stripe = new Stripe(config.stripeKey)
+  stripe = new Stripe(config.stripeKey)
 }
 
 const getAllDonations = async () => {
@@ -43,6 +44,29 @@ router.post('/', async (req, res) => {
             amount,
             email
         })
+        const nodemailer = require('nodemailer')
+        console.log(GMAIL_PASS, GMAIL_USER)
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+            auth: {
+              user: GMAIL_USER,
+              pass: GMAIL_PASS
+            }
+          })
+        const msgMail = `Te damos profundas gracias desde Mascotapp por colaborar. Nuestro proyecto necesita de las financiación de los usuarios por lo cual tu aporte es muy importante.`
+      
+        const mailOptions = {
+          from: 'service.mascotapp@gmail.com',
+          to: email,
+          subject: 'Donación recibida!',
+          html: `<div>${msgMail}</div><div>Monto donado: ${amount / 100} USD</div><div>ID de la transferencia: ${id}</div>`
+        }
+
+        transporter.sendMail(mailOptions, function(error: any, info: any) {
+          if(error) console.log(error)
+          else console.log('Email enviado: ' + info.response)
+        })
+
         console.log('donation: ' + donation)
         res.send({msg: 'Succesfull payment'})
 
