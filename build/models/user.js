d7b5b6432392dfa4c707aed875f73c947f5487b9
Robert0101 <rob.tangled@gmail.com>
2022-09-22 17:23:17 -0300

    perf/Retocar types de User para que id e email sean obligatorios

--- build/models/user.js
+++ build/models/user.js
@@ -30,7 +30,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         email: {
             type: DataTypes.STRING,
-            allowNull: true,
+            allowNull: false,
             unique: false,
         },
         city: {
