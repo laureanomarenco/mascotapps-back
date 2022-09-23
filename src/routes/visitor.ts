696664c06be960b0f84d612cf589bc931cba975f
Robert0101 <rob.tangled@gmail.com>
2022-09-23 09:34:26 -0300

    clog/Agregar clogs y try catch a GET "/visitor/"

--- src/routes/visitor.ts
+++ src/routes/visitor.ts
@@ -2,29 +2,32 @@ import { Router } from "express";
 import db from "../../models/index";
 import { visitor } from "../types/visitorTypes";
 
-const route = Router()
-
-
+const route = Router();
 
 route.get("/", async (req: any, res) => {
-      let newVisitor: visitor = {
-        id: undefined,
-      };
-      let newVisit = await db.Visitor.create(newVisitor);
-      res.send(newVisit + 'juka puto')
- }
-)
+  console.log(`Entré a /visitor`);
+  try {
+    let newVisitor: visitor = {
+      id: undefined,
+    };
+    let newVisit = await db.Visitor.create(newVisitor);
+    console.log(`Visita registrada en la DB`);
+    res.send(newVisit + "juka puto");
+  } catch (error: any) {
+    console.log(`Error en /visitor/`);
+    return error.message;
+  }
+});
 
-route.get("/numbervisitors", async (req:any, res:any) => {
-    console.log("Entré a /numbervisitors");
-    try {
-      let arrayVisitors = await db.Visitor.findAll();
-      let numberOfVisitors = arrayVisitors.length;
-      res.status(200).send(`${numberOfVisitors}`);
-    } catch (error) {
-      res.status(404).send(error);
-    }
-  });
+route.get("/numbervisitors", async (req: any, res: any) => {
+  console.log("Entré a /numbervisitors");
+  try {
+    let arrayVisitors = await db.Visitor.findAll();
+    let numberOfVisitors = arrayVisitors.length;
+    res.status(200).send(`${numberOfVisitors}`);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
- 
-  export default route;
+export default route;
