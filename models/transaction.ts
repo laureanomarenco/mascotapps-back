84e90635844c32ece0d2400fd8405170cf0815c5
Robert0101 <rob.tangled@gmail.com>
2022-09-24 02:10:20 -0300

    Crear model Transaction

---
+++ models/transaction.ts
@@ -0,0 +1,51 @@
+"use strict";
+
+import { UUIDV4 } from "sequelize";
+
+const { Model } = require("sequelize");
+module.exports = (sequelize: any, DataTypes: any) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models: any) {
+      // define association here
+    }
+  }
+  Transaction.init(
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: UUIDV4,
+      },
+      user_offering_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_demanding_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_offering_check: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_demanding_check: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Transaction",
+    }
+  );
+  return Transaction;
+};
