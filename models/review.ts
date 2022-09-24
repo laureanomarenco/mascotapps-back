fd8fb76a71d2f7c229bac32e9ec7e8fe16a1bdb2
Robert0101 <rob.tangled@gmail.com>
2022-09-24 01:58:50 -0300

    Configurar mejor el modelo Review

--- models/review.ts
+++ models/review.ts
@@ -1,4 +1,7 @@
 "use strict";
+
+import { UUIDV4 } from "sequelize";
+
 const { Model } = require("sequelize");
 module.exports = (sequelize: any, DataTypes: any) => {
   class Review extends Model {
@@ -13,12 +16,42 @@ module.exports = (sequelize: any, DataTypes: any) => {
   }
   Review.init(
     {
-      review_id: DataTypes.STRING,
-      transaction_id: DataTypes.STRING,
-      reviewer_id: DataTypes.STRING,
-      reviewed_id: DataTypes.STRING,
-      comments: DataTypes.STRING,
-      stars: DataTypes.STRING,
+      id: {
+        type: DataTypes.STRING,
+        defaultValue: UUIDV4,
+        primaryKey: true,
+      },
+      transaction_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      reviewer_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      reviewed_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      comments: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        validate: {
+          len: {
+            args: [0, 1000],
+            msg: "SQLZ Error. El largo debe ser menor a 1000 caracteres",
+          },
+        },
+      },
+      stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 1,
+          max: 5,
+        },
+      },
     },
     {
       sequelize,
