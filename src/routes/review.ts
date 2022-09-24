0168dfdd687b0ccee91f32a29879937b2824a407
Robert0101 <rob.tangled@gmail.com>
2022-09-24 03:50:49 -0300

    build/ Crear archivos de rutas para Reviews y Transactions

---
+++ src/routes/review.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import db from "../../models/index";
+
+const router = Router();
+
+//-----  FUNCIONES AUXILIARES: -------------------------------
+
+async function getAllReviews() {
+  try {
+    let allTheReviewsFromDB = await db.Review.findAll();
+    return allTheReviewsFromDB;
+  } catch (error: any) {
+    console.log(
+      `Error en function getAllReviews. Error message: ${error.message} `
+    );
+    throw new Error(error.message);
+  }
+}
+
+//------  RUTAS: -----------------------------------------------
+router.get("/allReviews", async (req, res) => {
+  console.log(`Entré a la ruta /reviews/allReviews`);
+  try {
+    let allReviews = await getAllReviews();
+    return res.status(200).send(allReviews);
+  } catch (error: any) {
+    console.log(`Error en /reviews/allReviews`);
+    return res.status(404).send(error.message);
+  }
+});
---
+++ src/routes/transaction.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import db from "../../models/index";
+
+const router = Router();
+
+//-----  FUNCIONES AUXILIARES: -------------------------------
+async function getAllTransactions() {
+  try {
+    let allTheTransactionsFromDB = await db.Transaction.findAll();
+    return allTheTransactionsFromDB;
+  } catch (error: any) {
+    console.log(
+      `Error en function getAllTransactions. Error message: ${error.message} `
+    );
+    throw new Error(error.message);
+  }
+}
+//------  RUTAS: -----------------------------------------------
+router.get("/allTransactions", async (req, res) => {
+  console.log(`Entré a la ruta /Transactions/allTransactions`);
+  try {
+    let allTransactions = await getAllTransactions();
+    return res.status(200).send(allTransactions);
+  } catch (error: any) {
+    console.log(`Error en /Transactions/allTransactions`);
+    return res.status(404).send(error.message);
+  }
+});
