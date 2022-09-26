b26e6cf919973c69fe6cc78fe7905f18fa246c87
laureanomarenco <laureanomarenco@gmail.com>
2022-09-25 21:13:28 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -17,15 +17,24 @@ const app = express();
 
 app.use(express.json());
 
-var corsOptions = {
-  origin: ["https://mascotapps.vercel.app", "http://localhost:3000"],
-  headers: "*", 
-  methods: "*",
-  credentials: true,
-};
+// var corsOptions = {
+//   origin: ["https://mascotapps.vercel.app", "http://localhost:3000"],
+//   headers: "*", 
+//   methods: "*",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-
+// app.use(cors(corsOptions));
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 // RUTAS:
 app.use("/users", usersRouter);
 app.use("/pets", animalRouter);
