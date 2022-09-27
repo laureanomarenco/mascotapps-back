ade9015d8c70514210f226f9244b66d74a2a6881
alejandro juka <alejandrojuka2@gmail.com>
2022-09-27 15:26:53 -0300

    arreglando errores

--- config/web_Push_setup.js
+++ config/web_Push_setup.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
-const {PUBLIC_KEY, PRIVATE_KEY} = process.env
+const {PRIVATE_KEY} = process.env
+
+PUBLIC_KEY = BBdVJOoIKvsEDiJcbrNBuakK2qOotO8f7j4mTfiAk25SH8z0OQcxKlkUb3NJnetnMy5DYk0d0Ns2ffC69zfcGrE
+
 const webPush = require("web-push")
 
 webPush.setVapidDetails("mailto:service.mascotapp@gmail.com", PUBLIC_KEY, PRIVATE_KEY)
