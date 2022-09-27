286a305de54330e085044d30416664d9a17f2ced
alejandro juka <alejandrojuka2@gmail.com>
2022-09-27 15:24:31 -0300

    arreglando errores

--- config/web_Push_setup.js
+++ config/web_Push_setup.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const {PUBLIC_KEY, PRIVATE_KEY} = process.env
 const webPush = require("web-push")
 
-webPush.setVapidDetails("service.mascotapp@gmail.com", PUBLIC_KEY, PRIVATE_KEY)
+webPush.setVapidDetails("mailto:service.mascotapp@gmail.com", PUBLIC_KEY, PRIVATE_KEY)
 
 module.exports = webPush
