24945baf5c9025bb9fdeb75046d8b9927db0e61f
alejandro juka <alejandrojuka2@gmail.com>
2022-09-27 16:28:53 -0300

    verificaciones rutas subscribe

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -491,6 +491,7 @@ router.get("/:id", async (req, res) => {
 
 let pushSubscription:any;
 router.post("/subscribe", async(req,res)=>{
+  console.log("entre a subscribe")
   pushSubscription = req.body
   res.status(200).json()
  
