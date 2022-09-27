8743a6e35989d66d3349a17d11f7e3efc7954e97
alejandro juka <alejandrojuka2@gmail.com>
2022-09-27 15:29:20 -0300

    agregando variables

--- config/web_Push_setup.js
+++ config/web_Push_setup.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
-const {PRIVATE_KEY} = process.env
-
-PUBLIC_KEY = BBdVJOoIKvsEDiJcbrNBuakK2qOotO8f7j4mTfiAk25SH8z0OQcxKlkUb3NJnetnMy5DYk0d0Ns2ffC69zfcGrE
-
+const {PUBLIC_KEY, PRIVATE_KEY} = process.env
 const webPush = require("web-push")
 
 webPush.setVapidDetails("mailto:service.mascotapp@gmail.com", PUBLIC_KEY, PRIVATE_KEY)
