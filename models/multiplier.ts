428c40a5a1eb7c2c7bb4f6bda454121e511e88cd
Robert0101 <rob.tangled@gmail.com>
2022-09-29 16:02:31 -0300

    fix/Fix model y ruta creadora de instancia Multiplier

--- models/multiplier.ts
+++ models/multiplier.ts
@@ -3,28 +3,31 @@
 import { MultiplierType } from "../src/types/multiplierTypes";
 const { Model } = require("sequelize");
 
-
 module.exports = (sequelize: any, DataTypes: any) => {
-    class Multiplier extends Model<MultiplierType> implements MultiplierType {
-      /**
-       * Helper method for defining associations.
-       * This method is not a part of Sequelize lifecycle.
-       * The `models/index` file will call this method automatically.
-       */
-      number!: number; // sub
-      }
-    Multiplier.init(
-      {
-        number: {
-          type: DataTypes.INTEGER,
-          defaultValue: 1,
-        },
+  class Multiplier extends Model<MultiplierType> implements MultiplierType {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    number!: number; // sub
+  }
+  Multiplier.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+        primaryKey: true,
       },
-      {
-        sequelize,
-        modelName: "Multiplier",
-      }
-    );
-    return Multiplier;
-  };
-  
+      number: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Multiplier",
+    }
+  );
+  return Multiplier;
+};
