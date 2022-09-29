b5fea166927706d9c4bb249a147a1571ea96a70c
Laureano Marenco <73677385+laureanomarenco@users.noreply.github.com>
2022-09-29 10:20:52 -0300

    Revert "Revert "aplicamos las notificaciones push porfavor que este bien cordialmente con mucho amor ale y ren""

---
+++ build/config/web_Push_setup.js
@@ -0,0 +1,6 @@
+"use strict";
+require("dotenv").config();
+const { PUBLIC_KEY, PRIVATE_KEY } = process.env;
+const webPush = require("web-push");
+webPush.setVapidDetails("mailto:service.mascotapp@gmail.com", PUBLIC_KEY, PRIVATE_KEY);
+module.exports = webPush;
