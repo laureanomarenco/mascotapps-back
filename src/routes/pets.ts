efcc1402505ca38e7f19dab0b4f49ae5ac93f8ad
alejandro juka <alejandrojuka2@gmail.com>
2022-09-30 01:43:10 -0300

    agregando variables entorno

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -553,11 +553,8 @@ router.post("/notify" ,async(req,res)=>{
       text: "Está perdido por tu zona,¿lo has visto?",
     }
     const string = JSON.stringify(payload)
-    console.log("soy string", string)
     webPush.sendNotification(pushSubscription, string)
-    console.log("estoy despues de sendNOtiifcationsa")
     res.status(200).json()
-    console.log("estoy despues de json")
   } catch (error) {
     console.log(error)
   }
